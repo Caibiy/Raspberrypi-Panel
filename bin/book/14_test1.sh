bc3140a1e2ca8f87cb6187b9f0d26b39c0c17420
Break.Yang <caibiy666@gmail.com>
2018-01-30 17:17:38 +0800

    some practice:sparkles:

---
+++ bin/book/14_test1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+factorial=1
+for(( number = 1;number <= $1 ; number++))
+do
+  factorial=$[ $factorial * $number ]
+done
+echo The factorial of $1 is $factorial
---
+++ bin/book/14_test2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+total=$[ $1 *$2 ]
+echo "The first param is $1"
+echo "The second param is $2"
+echo "The total is $total"
