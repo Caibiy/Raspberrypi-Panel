15ac1dab0481e38202f0512033bf0bc3697b3944
JiaDong.Yang <caibiy666@gmail.com>
2018-02-03 07:35:03 -0800

    完善wifi脚本:sparkles:

--- raspi/wifi.sh
+++ raspi/wifi.sh
@@ -2,13 +2,19 @@
 path=/etc/wpa_supplicant/wpa_supplicant.conf
 wifiname=$1
 wifipass=$2
+write_wifi(){
+     echo "network={
+ssid=\"${wifiname}\"
+psk=\"${wifipass}\"
+}" >>$path
+}
 if [ -e $path ];then
-   if [ -n $wifiname ]&& [ -n $wifipass ];then
-      echo "请输入wifi名和密码" 
-   else
-      echo "wifi名字:$wifiname,wifi密码$wifipass"
+   if [ -n $wifiname ] && [ -n $wifipass ];then
       echo "正在写入配置....."
-       
+      echo "wifi名字:$wifiname,wifi密码$wifipass"
+      write_wifi
+   else
+      echo "请输入wifi名和密码" 
    fi
 else
   echo "文件不存在"
