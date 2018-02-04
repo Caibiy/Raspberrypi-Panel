f879441aad2f59a1ad36b3c53f56f4f0c660f15f
JiaDong.Yang <caibiy666@gmail.com>
2018-02-04 06:46:40 -0800

    修改后台路径

--- index.js
+++ index.js
@@ -12,37 +12,43 @@ res.sendfile(path.join(__dirname,'index.html'))
 })
 //Show progress status
 app.get('/api/ps',(req,res)=>{
-  exec('sh ./bash/ps.sh',(err,stdout,stderr)=>{
+  exec('sh ./bin/ps.sh',(err,stdout,stderr)=>{
   res.json(callback(err,stdout,stderr));
 })
 })
 //Show current OS user
 app.get('/api/user',(req,res)=>{
-exec('sh ./bash/user.sh',(error,stdout,stderr)=>{
+exec('sh ./bin/user.sh',(error,stdout,stderr)=>{
     res.send(pug.renderFile("./view/user.pug",callback(error,stdout,stderr)))
 })
 })
 //Show current directory of file
 app.get('/api/ls',(req,res)=>{
- exec('sh ./bash/ls.sh',(error,stdout,stderr)=>{
+ exec('sh ./bin/ls.sh',(error,stdout,stderr)=>{
  res.json(callback(error,stdout,stderr));
 })
 })
 //Show Dir status
 app.get('/api/dstatus',(req,res)=>{
-exec('bash ./bash/dstatus.sh',(error,stdout,stderr)=>{
+exec('bash ./bin/dstatus.sh',(error,stdout,stderr)=>{
   res.json(callback(error,stdout,stderr));
 })
 })
 //显示文件在磁盘中的占用率
 app.get('/api/fstatus',(req,res)=>{
 if(req.query){
-exec("sh ./bash/fstatus.sh "+req.query.file,(error,stdout,stderr)=>{
+exec("sh ./bin/fstatus.sh "+req.query.file,(error,stdout,stderr)=>{
 res.json(callback(error,stdout,stderr));
 })
 }else{
 res.json({"eror":"没有传递参数"})
 }
+})
+//查询当前wifi是否已经配置
+app.get('/api/wifi',(req,res)=>{
+  exec("sh ./bin/wifista.sh",(error,stdout,stderr)=>{
+    res.json(callback(err,stdout,stderr));
+})
 })
 app.listen(8080,()=>{
 console.log("Server is running on port: 8080")
