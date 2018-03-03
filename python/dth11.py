cf1b47844110fc7ea12669905a22a0d70bf1cec5
caibiy <caibiy666@gmail.com>
2018-03-04 00:28:21 +0800

    rebuild project 2018年 03月 04日 星期日 00:28:21 CST

---
+++ python/dth11.py
@@ -0,0 +1,8 @@
+import sys
+import Adafruit_DHT
+
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
