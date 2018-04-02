43123270a95dbf89f8cbfa5a1f3f216ccbe8b697
LeeJay <caibiy666@gmail.com>
2018-04-02 16:59:42 +0800

    Update smarthome.py

--- python/smarthome.py
+++ python/smarthome.py
@@ -3,7 +3,7 @@
 
 __author__ = 'Caibiy'
 
-import Adafruit_DHT,os,time,datetime,sqlite3
+import Adafruit_DHT,os,time,datetime,sqlite3,uuid
 conn,cursor=(None,None)
 #gpio
 @unique
@@ -17,10 +17,30 @@ def initDb():
 	global conn,cursor
 	conn = sqlite3.connect("./db/smarthome.db")
 	cursor = conn.cursor()
-	#图片表如果不存在则创建
+	#图片表
 	cursor.execute('''CREATE TABLE IF NOT EXISTS pic (
-	 		picid  INT UNSIGNED AUTO_INCREMENT PRIMARY KEY ,
+	 		picid  INT UNSIGNED  PRIMARY KEY ,
 	 		time  VARCHAR (100) NOT NULL
+			)''')
+	#温度表
+	cursor.execute('''CREATE TABLE IF NOT EXISTS dth (
+	 		dthid  INT UNSIGNED  PRIMARY KEY ,
+	 		temp  VARCHAR (100) NOT NULL,
+			humidity VARCHAR(100) NOT NULL
+			)''')
+	#mq2表
+	cursor.execute('''CREATE TABLE IF NOT EXISTS mq2 (
+	 		mq2id  INT UNSIGNED  PRIMARY KEY ,
+	 		lpg  VARCHAR (100) NOT NULL,
+			co VARCHAR(100) NOT NULL,
+			smoke VARCHAR(100) NOT NULL
+			)''')
+	#操作记录表
+	cursor.execute('''CREATE TABLE IF NOT EXISTS operalog (
+	 		id  INT UNSIGNED  PRIMARY KEY ,
+			time VARCHAR(100) NOT NULL,
+			operaid VARCHAR(100) NOT NULL,
+			type VARCHAR(100) NOT NULL
 			)''')
 	conn.commit()
 #执行sql
@@ -34,24 +54,26 @@ class smartHome(object):
 	@property
 	def sensor(self):
 		return self.sensor
-	def __init__(self,pdht11,pmq2,pbuzzer):
-		self.__pdht11 = pdht11
-		self.__pmq2 = pmq2
-		self.__pbuzzer = pbuzzer
+	def __init__(self):
 		#初始化mode
-		os.system('gpio -g mode %s out' % pdht11)
-		os.system('gpio -g mode %s out' % pmq2)
-		os.system('gpio -g mode %s out' % pbuzzer)
+		os.system('gpio -g mode %s out' % gpio.pdht11.value)
+		os.system('gpio -g mode %s out' % gpio.pmq2.value)
+		os.system('gpio -g mode %s out' % gpio.pbuzzer.value)
 	#蜂鸣器
 	def buzzer(self):
-		os.system('gpio -g write %s 1' % self.__pbuzzer)
+		os.system('gpio -g write %s 1' % gpio.pbuzzer.value)
 		time.sleep(0.5)
-		os.system('gpio -g write %s 0' % self.__pbuzzer)
+		os.system('gpio -g write %s 0' % gpio.pbuzzer.value)
+	#读取dth11温度
 	def readDth(self):
-		humidity,temperature = Adafruit_DHT.read_retry(self.sensor,self.__pdht11)
+		humidity,temperature = Adafruit_DHT.read_retry(self.sensor,gpio.pdht11.value)
 		if humidity is not None and temperature is not None:
 			print('temp:%s,humidity%d' % (humidity,temperature))
+			saveDth(temperature,humidity)
 			return (humidity,temperature)
+	def saveDth(self,temp,humidity):
+		executeDb('insert into dth (time) VALUES (\'%s\',\'%s\') ' %  (temp,humidity))
+		
 #USB摄像头
 class usbCamera(object):
 	def __init__(self):
@@ -59,15 +81,16 @@ class usbCamera(object):
 	def takePhoto(self):
 		nowTime = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
 		os.system('fswebcam  -r 1280x720 --no-banner ../img/%s.jpg' % nowTime)
-		executeDb('insert into pic (time) VALUES (\'%s\') ' %  nowTime)
+		saveTime(nowTime)
+	def saveTime(self,time):
+		executeDb('insert into pic (time) VALUES (\'%s\') ' %  time)
 def init():
 	os.system('bash ./check.sh')
 	s = smartHome(23,11,21)
 	initDb()
 	#uc = usbCamera()
 	#uc.takePhoto()	
-	cursor.execute('select * from pic')
-	print(cursor.fetchall())
+	
 
 if __name__ =='__main__':
 	init()
